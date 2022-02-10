158ccba2778775d861d3bac2c12cdccc490db7d0
zahra <z.sh.moeini1379@gmail.com>
2022-02-10 16:29:15 +0330

    10feb2

--- 10feb/js/News.js
+++ 10feb/js/News.js
@@ -7,5 +7,32 @@ class News{
     queryAPI(newsNams, country, category){
         //bulid url
         let url = "https://newsapi.org/v2/";
+        //check to country and country value
+        if(country==='' && category===''){
+            url+='everything?'
+        }
+        else{
+            url+='top-headlines?'
+        }
+
+
+        //if newsname  exist 
+        if(newsNams !==''){
+            url+=`q=${newsNams}&`
+        }
+
+
+        //if country exiset
+        if(country !==''){
+            url+=`country=${country}&`
+        }
+        //if category exist
+        if(category !==''){
+            url+=`category${category}&`
+        }
+
+        //comp;ete url with key
+        url+=`apiKey=${this.APIkey}`
+        console.log(url);
     }
 }
