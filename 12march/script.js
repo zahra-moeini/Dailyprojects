bdab9bf7b6d00e341cd8dd1e3e6a50e34109b4ac
zahra <z.sh.moeini1379@gmail.com>
2022-03-13 15:24:41 +0330

    12marchcomit

--- 12march/script.js
+++ 12march/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("2022-3-13 00:31:20");
+var countDownDate = new Date("2022-3-13 15:22:20");
 var x=setInterval(function (){
     var now=new Date().getTime();
     var distance=countDownDate-now;
@@ -11,14 +11,14 @@ var x=setInterval(function (){
     .innerHTML =days + "Day" +"  "  +hours +"hours" +" "+ minutes +"minutes" +" "+ seconds +"seconds";
     function secondess(){
         if (days == 0 && hours == 0 && minutes==0 &&seconds<10) {
-            document.getElementById("demo").style.color="green";
+            document.getElementById("demo").style.color="green"
         }
     }
      secondess();
    
      if(distance<0){
          clearInterval(x);
-        document.getElementById("demo").innerHTML = ".سال 1401 هجری شمسی  مبارک باد";
+      document.getElementById("demo").innerHTML = ".سال 1401 هجری شمسی  مبارک باد";
+        
     }
 },1000);
-
