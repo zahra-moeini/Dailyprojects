f934b5a30c98e29cce64aed11e24f13e4ea6f36b
zahra <z.sh.moeini1379@gmail.com>
2022-03-17 12:02:50 +0330

    17maechhh

--- 12march/script.js
+++ 12march/script.js
@@ -1,35 +1,24 @@
-var countDownDate = new Date("2022-3-13 16:26:59");
+// 2022-3-20 19:03:00
+var countDownDate = new Date("2022-3-20 19:03:00");
 var x=setInterval(function (){
     var now=new Date().getTime();
     var distance=countDownDate-now;
     var days=Math.floor(distance/(1000*60*60*24));
     var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60))
     var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
-    var seconds=Math.floor((distance%(1000*60))/1000);
-   
+    var seconds=Math.floor((distance%(1000*60))/1000); 
     document.getElementById("demo")
     .innerHTML =days + "Day" +"  "  +hours +"hours" +" "+ minutes +"minutes" +" "+ seconds +"seconds";
-    function secondess(){
-        if (days == 0 && hours == 0 && minutes==0 &&seconds<10) {
-            document.getElementById("demo").style.color="green"
-        }
+    // function color text
+    http: function secondess() {
+      if (days == 0 && hours == 0 && minutes == 0 && seconds < 10) {
+        document.getElementById("demo").style.color = "green";
+      }
     }
-    secondess();
-    function back(){
-        
-        document.body.style.backgroundColor="red"
-    }
-    function daa(){
-        if(days == 0 && hours == 0 && minutes==0 &&seconds<59){
-           
-            
-        }
-    }
-    
-    daa();
+    secondess(); 
     if(distance<0){
         clearInterval(x);
       document.getElementById("demo").innerHTML = ".سال 1401 هجری شمسی  مبارک باد";
-       back();
+       
     }
 },1000);
